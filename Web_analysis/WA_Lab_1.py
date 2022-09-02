85d4ee72890e3721106e3b267976d34f10a30e2a
Baster <Baster@DESKTOP-FFTVG2S>
2022-09-02 22:24:49 +0600

    Friday 02 commit

--- Web_analysis/WA_Lab_1.py
+++ Web_analysis/WA_Lab_1.py
@@ -11,6 +11,14 @@ vk = vk_session.get_api()
 target_group_id = 197217619
 
 
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        func(*args, **kwargs)
+        print(time.time() - start_time)
+    return wrapper
+
+
 def get_group_members(group_id: int, save_file: bool = False):
     # Начало
     user_count = vk.groups.getMembers(group_id=target_group_id)["count"]
@@ -33,12 +41,9 @@ def get_group_members(group_id: int, save_file: bool = False):
 
 
 def get_users_groups_member_count(user_list: list = None, from_file: bool = False, save_csv: bool = False):
-    start_time = time.time()
-
     if from_file:
         with open("group_users.txt", "r") as f:
             user_list = list(map(int, f.read().split(", ")))
-    count = 0
     # Начало
     # Создаем словарь что бы сохранять только уникальные группы
     users_groups = dict()
@@ -53,10 +58,6 @@ def get_users_groups_member_count(user_list: list = None, from_file: bool = Fals
                         continue
         except vk_api.exceptions.ApiError:
             continue
-        finally:  # Не обязательно
-            count += 1
-            if count % 100 == 0:
-                print(f"{count} users processed for {(time.time() - start_time)} second")
     group_id_list, name_list, members_count_list = [], [], []
     for item in users_groups.items():
         group_id_list.append(item[0])
@@ -71,47 +72,50 @@ def get_users_groups_member_count(user_list: list = None, from_file: bool = Fals
     return users_groups
 
 
+def get_age(bdate: str):
+    if re.match("^[0-9]{1,2}.[0-9]{1,2}.[0-9]{4}$", bdate) is not None:
+        _date = datetime.strptime(bdate, "%d.%m.%Y")
+        return date.today().year - _date.year - ((date.today().month, date.today().day) < (_date.month, _date.day))
+    else:
+        return -1
+
+
+@timer
 def get_group_members_info(group_id: int, save_csv: bool = False):
-    count = 0
-    start_time = time.time()
     # Начало
+    user_id_list, inactive_list, sex_list, age_list, country_list, city_list = [], [], [], [], [], []
     user_list = get_group_members(group_id)
     members_count = len(user_list)
-    date_pattern = "^[0-9]{1,2}.[0-9]{1,2}.[0-9]{4}$"
-    today = date.today()
-    inactive_list, sex_list, age_list, country_list, city_list = [], [], [], [], []
-    for user in user_list:
-        user_info = vk.users.get(user_ids=[user], fields=["city", "county", "sex", "bdate"])[0]
-        if "deactivated" in user_info:
-            inactive_list.append(True)
-            sex_list.append(-1)
-            age_list.append(-1)
-            country_list.append(None)
-            city_list.append(None)
-            continue
-        inactive_list.append(False)
-        sex_list.append(user_info["sex"])
-        if "bdate" in user_info and re.match(date_pattern, user_info["bdate"]) is not None:
-            bdate = datetime.strptime(user_info["bdate"], "%d.%m.%Y")
-            age = today.year - bdate.year - ((today.month, today.day) < (bdate.month, bdate.day))
-            age_list.append(age)
-        else:
-            age_list.append(-1)
-        if "country" in user_info:
-            country_list.append(user_info["country"]["title"])
-        else:
-            country_list.append(None)
-        if "city" in user_info:
-            city_list.append(user_info["city"]["title"])
-        else:
-            city_list.append(None)
-        # Не обязательно
-        count += 1
-        if count % 100 == 0:
-            print(f"{count} users processed for {(time.time() - start_time)} second")
+    user_list = [user_list[i:i + 1000] for i in range(0, len(user_list), 1000)]
+    for part in user_list:
+        fetched_data = vk.users.get(user_ids=part, fields=["city", "county", "sex", "bdate"])
+        for user_info in fetched_data:
+            user_id_list.append(user_info["id"])
+            if "deactivated" in user_info:
+                inactive_list.append(True)
+                sex_list.append(-1)
+                age_list.append(-1)
+                country_list.append(None)
+                city_list.append(None)
+                continue
+            inactive_list.append(False)
+            sex_list.append(user_info["sex"])
+            if "bdate" in user_info:
+                age = get_age(user_info["bdate"])
+                age_list.append(age)
+            else:
+                age_list.append(-1)
+            if "country" in user_info:
+                country_list.append(user_info["country"]["title"])
+            else:
+                country_list.append(None)
+            if "city" in user_info:
+                city_list.append(user_info["city"]["title"])
+            else:
+                city_list.append(None)
     dataframe = pd.DataFrame(
         {"inactive": inactive_list, "sex": sex_list, "age": age_list, "country": country_list, "city": city_list},
-        index=user_list)
+        index=user_id_list)
     # Конец return members_count, df
     if save_csv:
         dataframe.to_csv(f"./group_members{members_count}_info")
@@ -124,6 +128,4 @@ if __name__ == "__main__":
     # get_users_groups_member_count(from_file=True, save_csv=True)
     # print(pd.read_csv("groups_member_count_archive", index_col=0).head(20))
     # part 2
-    m, df = get_group_members_info(target_group_id)
-    print(m)
-    print(df)
+    get_group_members_info(target_group_id, save_csv=True)
