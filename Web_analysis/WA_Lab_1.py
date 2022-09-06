c5d16575bf7ac3fac50bc5312d572d39ba6c44b5
Buk <aek33@tpu.ru>
2022-09-06 17:01:06 +0600

    Task 2 complited

--- Web_analysis/WA_Lab_1.py
+++ Web_analysis/WA_Lab_1.py
@@ -1,8 +1,11 @@
 import time
 import re
 from datetime import date, datetime
+from collections import Counter
+
 import vk_api
 import pandas as pd
+
 from config import VK_API_1
 
 vk_session = vk_api.VkApi(token=VK_API_1)
@@ -15,7 +18,7 @@ def timer(func):
     def wrapper(*args, **kwargs):
         start_time = time.time()
         func(*args, **kwargs)
-        print(time.time() - start_time)
+        print(f"Execution time: {time.time() - start_time}")
     return wrapper
 
 
@@ -40,10 +43,13 @@ def get_group_members(group_id: int, save_file: bool = False):
         return user_list
 
 
-def get_users_groups_member_count(user_list: list = None, from_file: bool = False, save_csv: bool = False):
+@timer
+def get_users_groups_member_count(group_id: int, from_file: bool = False, save_csv: bool = False):
     if from_file:
         with open("group_users.txt", "r") as f:
             user_list = list(map(int, f.read().split(", ")))
+    else:
+        user_list = get_group_members(group_id)
     # Начало
     # Создаем словарь что бы сохранять только уникальные группы
     users_groups = dict()
@@ -66,6 +72,7 @@ def get_users_groups_member_count(user_list: list = None, from_file: bool = Fals
     df = pd.DataFrame({"name": name_list, "members_count": members_count_list},
                       index=group_id_list)
     df = df.sort_values(by=["members_count"], ascending=False)
+    print(f"Топ-10 групп по числу участников\n{df.head(10)}")
     # Конец return df
     if save_csv:
         df.to_csv("./members_groups_member_count")
@@ -81,51 +88,80 @@ def get_age(bdate: str):
 
 
 @timer
-def get_group_members_info(group_id: int, save_csv: bool = False):
+def get_group_members_info(group_id: int, from_file: bool = False, save_csv: bool = False):
+    if from_file:
+        dataframe = pd.read_csv("group_members_info")
+    else:
     # Начало
-    user_id_list, inactive_list, sex_list, age_list, country_list, city_list = [], [], [], [], [], []
-    user_list = get_group_members(group_id)
-    members_count = len(user_list)
-    user_list = [user_list[i:i + 1000] for i in range(0, len(user_list), 1000)]
-    for part in user_list:
-        fetched_data = vk.users.get(user_ids=part, fields=["city", "county", "sex", "bdate"])
-        for user_info in fetched_data:
-            user_id_list.append(user_info["id"])
-            if "deactivated" in user_info:
-                inactive_list.append(True)
-                sex_list.append(-1)
-                age_list.append(-1)
-                country_list.append(None)
-                city_list.append(None)
-                continue
-            inactive_list.append(False)
-            sex_list.append(user_info["sex"])
-            if "bdate" in user_info:
-                age = get_age(user_info["bdate"])
-                age_list.append(age)
-            else:
-                age_list.append(-1)
-            if "country" in user_info:
-                country_list.append(user_info["country"]["title"])
-            else:
-                country_list.append(None)
-            if "city" in user_info:
-                city_list.append(user_info["city"]["title"])
-            else:
-                city_list.append(None)
-    dataframe = pd.DataFrame(
-        {"inactive": inactive_list, "sex": sex_list, "age": age_list, "country": country_list, "city": city_list},
-        index=user_id_list)
-    # Конец return members_count, df
-    if save_csv:
-        dataframe.to_csv(f"./group_members{members_count}_info")
-    return members_count, dataframe
+        user_id_list, active_list, sex_list, age_list, country_list, city_list = [], [], [], [], [], []
+        user_list = get_group_members(group_id)
+        user_list = [user_list[i:i + 1000] for i in range(0, len(user_list), 1000)]
+        for part in user_list:
+            fetched_data = vk.users.get(user_ids=part, fields=["city", "county", "sex", "bdate"])
+            for user_info in fetched_data:
+                user_id_list.append(user_info["id"])
+                if "deactivated" in user_info:
+                    active_list.append(False)
+                    sex_list.append(-1)
+                    age_list.append(-1)
+                    country_list.append(None)
+                    city_list.append(None)
+                    continue
+                active_list.append(True)
+                sex_list.append(user_info["sex"])
+                if "bdate" in user_info:
+                    age = get_age(user_info["bdate"])
+                    age_list.append(age)
+                else:
+                    age_list.append(-1)
+                if "country" in user_info:
+                    country_list.append(user_info["country"]["title"])
+                else:
+                    country_list.append(None)
+                if "city" in user_info:
+                    city_list.append(user_info["city"]["title"])
+                else:
+                    city_list.append(None)
+        dataframe = pd.DataFrame(
+            {"active": active_list, "sex": sex_list, "age": age_list, "country": country_list, "city": city_list},
+            index=user_id_list)
+        # Если нужно сохранить данные
+        if save_csv:
+            dataframe.to_csv(f"./group_members_info")
+    # Блок обработки
+    members_count = len(dataframe)
+    dataframe = dataframe.loc[dataframe["active"]]
+    data_size = len(dataframe)
+    inactive_user_count = members_count - data_size
+
+    age_frame = dataframe["age"][dataframe["age"] != -1]
+    mean_age = age_frame.describe()["mean"]
+    median_age = age_frame.describe()["50%"]
+    hidden_age_count = data_size - len(age_frame)
+
+    sex_percentages = dataframe.groupby(by="sex")["sex"].count().transform(
+        (lambda x: round(x * 100 / data_size, 2)))
+
+    hidden_location_count = len(dataframe[dataframe["city"].isna()])
+    location_counter = Counter(dataframe["city"][dataframe["city"].notna()])
+    location_dataframe = pd.DataFrame.from_dict(location_counter, orient="index",
+                                                columns=["count"]).sort_values(by="count", ascending=False)
+
+    info_str = f"В группе {members_count} участников из них {inactive_user_count} удалено или заблокировано.\n\n" \
+               f"Средний возраст участников группы {int(mean_age)} лет;\n" \
+               f"медианное значение {int(median_age)} лет;\n" \
+               f"возраст не указан у {int(hidden_age_count * 100 / data_size)} % участников.\n\n" \
+               f"В группе {int(sex_percentages[2])} % мужчин, {int(sex_percentages[1])} % женщин" \
+               f" и {int(sex_percentages[0])} % участников не указали пол.\n\n" \
+               f"Из участников {round(hidden_location_count * 100 / data_size)} % не указали город проживания;\n" \
+               f"топ-10 по городам из указавших город проживания:\n{location_dataframe.head(10)}"
+    print(info_str)
+    return info_str
 
 
 if __name__ == "__main__":
     # part 1
-    # get_group_members(target_group_id, save_file=True)
-    # get_users_groups_member_count(from_file=True, save_csv=True)
-    # print(pd.read_csv("groups_member_count_archive", index_col=0).head(20))
+    # get_users_groups_member_count(group_id=target_group_id, from_file=True, save_csv=True)
     # part 2
-    get_group_members_info(target_group_id, save_csv=True)
+    get_group_members_info(group_id=target_group_id, from_file=True, save_csv=True)
+    # part 3
