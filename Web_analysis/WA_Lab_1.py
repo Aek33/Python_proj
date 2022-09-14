48f3abc622c839e252c634a1ce94e73d6e958587
Buk <aek33@tpu.ru>
2022-09-14 17:59:40 +0600

    async update

--- Web_analysis/WA_Lab_1.py
+++ Web_analysis/WA_Lab_1.py
@@ -1,39 +1,33 @@
-from time import perf_counter
+import time
 import re
 from datetime import date, datetime
 from collections import Counter
 import multiprocessing as mp
-import aiohttp
-import asyncio
+import matplotlib.pyplot as plt
+
 import vk_api
 import pandas as pd
 import networkx as nx
+
 from config import VK_TOKENS
 
-import dash
-import dash_cytoscape as cyto
-from dash import html
-
-
-vk_session = vk_api.VkApi(token=VK_TOKENS[3])
+vk_session = vk_api.VkApi(token=VK_TOKENS[0])
 vk = vk_session.get_api()
-app = dash.Dash(__name__)
+
 target_group_id = 197217619
 
 
 def timer(func):
-    """Декортаор для тестов"""
     def wrapper(*args, **kwargs):
-        start_time = perf_counter()
-        var = func(*args, **kwargs)
-        print(f"Execution time: {perf_counter() - start_time}")
-        return var
+        start_time = time.time()
+        func(*args, **kwargs)
+        print(f"Execution time: {time.time() - start_time}")
+
     return wrapper
 
 
-@timer
 def get_group_members(group_id: int, save_file: bool = False):
-    """id всех пользователей группы. vk.groups.getMembers() имеет ограничение в 1000 id за раз"""
+    # Начало
     user_count = vk.groups.getMembers(group_id=target_group_id)["count"]
     offset = 0
     user_set = set()
@@ -48,32 +42,34 @@ def get_group_members(group_id: int, save_file: bool = False):
         with open("group_users.txt", "w") as f:
             s = ', '.join(map(str, user_list))
             f.write(s)
-    return user_list
+        return "./group_users.txt"
+    else:
+        return user_list
 
 
 @timer
 def get_users_groups_member_count(group_id: int, from_file: bool = False, save_csv: bool = False):
-    """Функция для исследования количества пользователей в группах пользователей исследуемой группы"""
     if from_file:
         with open("group_users.txt", "r") as f:
             user_list = list(map(int, f.read().split(", ")))
     else:
         user_list = get_group_members(group_id)
+    # Начало
     # Создаем словарь что бы сохранять только уникальные группы
-    users_groups_dict = dict()
+    users_groups = dict()
     for user_id in user_list:
         try:
             user_info = vk.groups.get(user_id=user_id, extended=1, fields="members_count")
             for item in user_info["items"]:
                 if item["is_closed"] == 0:
                     try:
-                        users_groups_dict[item["id"]] = [item["name"], item["members_count"]]
+                        users_groups[item["id"]] = [item["name"], item["members_count"]]
                     except KeyError:
                         continue
         except vk_api.exceptions.ApiError:
             continue
     group_id_list, name_list, members_count_list = [], [], []
-    for item in users_groups_dict.items():
+    for item in users_groups.items():
         group_id_list.append(item[0])
         name_list.append(item[1][0])
         members_count_list.append(item[1][1])
@@ -84,11 +80,10 @@ def get_users_groups_member_count(group_id: int, from_file: bool = False, save_c
     # Конец return df
     if save_csv:
         df.to_csv("./members_groups_member_count.csv")
-    return users_groups_dict
+    return users_groups
 
 
 def get_age(bdate: str):
-    """Рассчет возраста по полной дате, если год не указан вернет -1"""
     if re.match("^[0-9]{1,2}.[0-9]{1,2}.[0-9]{4}$", bdate) is not None:
         _date = datetime.strptime(bdate, "%d.%m.%Y")
         return date.today().year - _date.year - ((date.today().month, date.today().day) < (_date.month, _date.day))
@@ -97,140 +92,120 @@ def get_age(bdate: str):
 
 
 @timer
-def get_group_members_info(group_id: int, save_csv: bool = False):
-    """Функция для сбора информации о пользовтелях группы"""
-    user_id_list, active_list, sex_list, age_list, country_list, city_list = [], [], [], [], [], []
-    user_list = get_group_members(group_id)
-    user_list = [user_list[i:i + 1000] for i in range(0, len(user_list), 1000)]
-    for part in user_list:
-        fetched_data = vk.users.get(user_ids=part, fields=["city", "county", "sex", "bdate"])
-        for user_info in fetched_data:
-            user_id_list.append(user_info["id"])
-            if "deactivated" in user_info:
-                active_list.append(False)
-                sex_list.append(-1)
-                age_list.append(-1)
-                country_list.append(None)
-                city_list.append(None)
-                continue
-            active_list.append(True)
-            sex_list.append(user_info["sex"])
-            if "bdate" in user_info:
-                age = get_age(user_info["bdate"])
-                age_list.append(age)
-            else:
-                age_list.append(-1)
-            if "country" in user_info:
-                country_list.append(user_info["country"]["title"])
-            else:
-                country_list.append(None)
-            if "city" in user_info:
-                city_list.append(user_info["city"]["title"])
-            else:
-                city_list.append(None)
-    dataframe = pd.DataFrame(
-        {"active": active_list, "sex": sex_list, "age": age_list, "country": country_list, "city": city_list},
-        index=user_id_list)
-    # Если нужно сохранить данные
-    if save_csv:
-        dataframe.to_csv(f"./group_members_info.csv")
-    return dataframe
-
-
-def show_group_members_info(dataframe: pd.DataFrame):
-    """Функция вывода отчета с информацией о группе на основе датафрейма"""
+def get_group_members_info(group_id: int, from_file: bool = False, save_csv: bool = False):
+    if from_file:
+        dataframe = pd.read_csv("group_members_info.csv")
+    else:
+        # Начало
+        user_id_list, active_list, sex_list, age_list, country_list, city_list = [], [], [], [], [], []
+        user_list = get_group_members(group_id)
+        user_list = [user_list[i:i + 1000] for i in range(0, len(user_list), 1000)]
+        for part in user_list:
+            fetched_data = vk.users.get(user_ids=part, fields=["city", "county", "sex", "bdate"])
+            for user_info in fetched_data:
+                user_id_list.append(user_info["id"])
+                if "deactivated" in user_info:
+                    active_list.append(False)
+                    sex_list.append(-1)
+                    age_list.append(-1)
+                    country_list.append(None)
+                    city_list.append(None)
+                    continue
+                active_list.append(True)
+                sex_list.append(user_info["sex"])
+                if "bdate" in user_info:
+                    age = get_age(user_info["bdate"])
+                    age_list.append(age)
+                else:
+                    age_list.append(-1)
+                if "country" in user_info:
+                    country_list.append(user_info["country"]["title"])
+                else:
+                    country_list.append(None)
+                if "city" in user_info:
+                    city_list.append(user_info["city"]["title"])
+                else:
+                    city_list.append(None)
+        dataframe = pd.DataFrame(
+            {"active": active_list, "sex": sex_list, "age": age_list, "country": country_list, "city": city_list},
+            index=user_id_list)
+        # Если нужно сохранить данные
+        if save_csv:
+            dataframe.to_csv(f"./group_members_info.csv")
+    # Блок обработки
     members_count = len(dataframe)
     dataframe = dataframe.loc[dataframe["active"]]
     data_size = len(dataframe)
     inactive_user_count = members_count - data_size
+
     age_frame = dataframe["age"][dataframe["age"] != -1]
     mean_age = age_frame.describe()["mean"]
     median_age = age_frame.describe()["50%"]
     hidden_age_count = data_size - len(age_frame)
+
     sex_percentages = dataframe.groupby(by="sex")["sex"].count().transform(
         (lambda x: round(x * 100 / data_size, 2)))
+
     hidden_location_count = len(dataframe[dataframe["city"].isna()])
     location_counter = Counter(dataframe["city"][dataframe["city"].notna()])
     location_dataframe = pd.DataFrame.from_dict(location_counter, orient="index",
                                                 columns=["count"]).sort_values(by="count", ascending=False)
+
     info_str = f"В группе {members_count} участников из них {inactive_user_count} удалено или заблокировано.\n\n" \
                f"Средний возраст участников группы {int(mean_age)} лет;\n" \
                f"медианное значение {int(median_age)} лет;\n" \
                f"возраст не указан у {int(hidden_age_count * 100 / data_size)} % участников.\n\n" \
-               f"В группе {int(sex_percentages[2])} % мужчин, {int(sex_percentages[1])} % женщин.\n" \
-               f"Пол не указан у {int(sex_percentages[0])} % участников.\n\n" \
+               f"В группе {int(sex_percentages[2])} % мужчин, {int(sex_percentages[1])} % женщин" \
+               f" и {int(sex_percentages[0])} % участников не указали пол.\n\n" \
                f"Из участников {round(hidden_location_count * 100 / data_size)} % не указали город проживания;\n" \
                f"топ-10 по городам из указавших город проживания:\n{location_dataframe.head(10)}"
     print(info_str)
     return info_str
 
 
-def get_users_friends(group_id: int, save_csv: bool = False):
-    """Функция для сбора id друзей пользвателей группы"""
-    user_list = get_group_members(group_id)
-    print("Harvest start!")
-
-    async def _get_user_friends(user_id: int):
-        try:
-            items = await vk.friends.get(user_id=user_id)['items']
-            print(user_id)
-            return {user_id, items}
-        except vk_api.exceptions.ApiError:
-            items = [-1]
-            return {user_id, items}
-
-    results = []
-
-    async def main(users):
-        for user in users:
-            results.append(_get_user_friends(user))
-        await asyncio.gather(*results)
-        return results
-    asyncio.run(main(user_list))
-    print("Harvest complete!")
-    users_friends_dict = {}
-
-    def _update_dict(pair):
-        print(pair.keys())
-        return users_friends_dict.update(pair)
-    pool = mp.Pool(4)
-    results = pool.map(_update_dict, [result for result in results])
-    pool.close()
-    print(users_friends_dict)
-    if save_csv:
-        pd.Series(users_friends_dict).to_csv("members_friends.csv")
-    return users_friends_dict
+def get_users_friends(user_id: int):
+    try:
+        _id = user_id
+        _items = vk.friends.get(user_id=user_id)['items']
+        return _id, _items
+    except vk_api.exceptions.ApiError:
+        _id = user_id
+        _items = [-1]
+        return _id, _items
 
 
 @timer
-def create_group_graph(users_friends_dict: dict):
-    """ва"""
-    # users_friends_dict = pd.read_csv("members_friends.csv", index_col=[0], converters={"0": pd.eval})["0"].to_dict()
-    user_list = list(users_friends_dict.keys())
+def create_group_graph(group_id: int = None, from_file: bool = False, save_csv: bool = False):
+    if from_file:
+        users_friends_dict = pd.read_csv("members_friends.csv", index_col=[0], converters={"0": pd.eval})["0"].to_dict()
+        user_list = users_friends_dict.keys()
+    elif group_id is not None:
+        user_list = get_group_members(group_id)
+        pool = mp.Pool(4)
+        results = pool.map(get_users_friends, [user for user in user_list])
+        pool.close()
+        users_friends_dict = {}
+        for result in results:
+            users_friends_dict[result[0]] = result[1]
+        if save_csv:
+            pd.Series(users_friends_dict).to_csv("members_friends.csv")
+    else:
+        return -1
+    count = 0
     g = nx.Graph(directed=False)
     for user in users_friends_dict:
         g.add_node(user)
         for friend in users_friends_dict[user]:
             if user != friend and user in user_list and friend in user_list:
                 g.add_edge(user, friend)
-    to_del = [i for i in g if g.degree(i) < 1]
-    for i in to_del:
-        del users_friends_dict[i]
-        user_list.remove(i)
-        g.remove_node(i)
-    # pos = nx.spring_layout(g)
+                count += 1
     # net = Network(height="1000px", width="1000px")
     # net.from_nx(g)
     # net.show("graph.html")
-    elements = []
-    for user in users_friends_dict:
-        elements.append({'data': {'id': str(user), 'label': str(user)}})
-        # 'preset': {'x': int(pos[user][0] * 1000), 'y': int(pos[user][1] * 1000)}})
-        for friend in users_friends_dict[user]:
-            if user != friend and user in user_list and friend in user_list:
-                elements.append({'data': {'source': str(user), 'target': str(friend)}})
-    # plt.show()
-    return elements
+    print(count)
+    nx.draw_spring(g)
+    plt.show()
+    return users_friends_dict
 
 
 if __name__ == "__main__":
@@ -239,17 +214,4 @@ if __name__ == "__main__":
     # part 2
     # get_group_members_info(group_id=target_group_id, from_file=True, save_csv=True)
     # part 3
-    # get_users_friends(target_group_id, save_csv=True)
-    # graph_data = create_group_graph(from_file=True, save_csv=True)
-    get_group_members(target_group_id)
-    # app.layout = html.Div([
-    #     cyto.Cytoscape(
-    #         id='vk-group-members-data',
-    #         layout={'name': 'cose'},
-    #         style={'width': '100%', 'height': '1000px'},
-    #         elements=graph_data
-    #     )
-    # ])
-    # app.run_server(debug=True)
-    print("See, sometimes, in life, you try and hit life with a 'hoocha!', but then life decides to hit you with a "
-          "'hoocha!'. You know what you do, in this predicament? When life 'hoochas' you? You go AGAIN! (C) Dum Shark")
+    create_group_graph(from_file=True, save_csv=True)
--- Web_analysis/vkAPI.py
+++ Web_analysis/vkAPI.py
@@ -1,4 +1,4 @@
-from time import perf_counter, sleep
+from time import perf_counter
 import multiprocessing as mp
 
 import asyncio
@@ -13,36 +13,43 @@ nest_asyncio.apply()
 def cutter(lst: list, n): return [lst[i:i + n] for i in range(0, len(lst), n)]
 
 
-async def execute_request(session, req, access_token):
+async def execute_request(session, req, access_token, sem):
     url = "https://api.vk.com/method/execute?"
     req = ",".join(req)
     data = dict(code=f'return[{req}];', access_token=access_token, v='5.131')
-    async with session.post(url, data=data) as resp:
-        response = await resp.json()
-        if 'error' in response:
-            sleep(0.5)
-            return execute_request(session, req, access_token)
-        return response
+    async with sem:
+        async with session.post(url, data=data) as resp:
+            response = await resp.json()
+            if 'error' in response:
+                await asyncio.sleep(0.5)
+                return await execute_request(session, req, access_token, sem)
+            return response
 
 
-async def process_execute(query: list, token):
+async def process_execute(query: list, tokens: list):
+    limit = asyncio.Semaphore(3)
     async with ClientSession() as session:
         tasks = []
         for part in query:
-            tasks.append(asyncio.ensure_future(execute_request(session, part, token)))
+            token = tokens.pop(0)
+            tasks.append(asyncio.ensure_future(execute_request(session, part, token, limit)))
+            tokens.append(token)
         return await asyncio.gather(*tasks)
 
 
 def get_group_members(group_id: int, tokens: list):
     start_time = perf_counter()
-    request = f"https://api.vk.com/method/groups.getMembers?group_id={target_group_id}&access_token={tokens[1]}&v=5.131"
+    request = f"https://api.vk.com/method/groups.getMembers?group_id={group_id}&access_token={tokens[0]}&v=5.131"
+    user_count = requests.get(request).json()
+    if 'error' in user_count:
+        raise Exception(f"VK API error\n{user_count}")
     user_count = requests.get(request).json()['response']['count']
     offset_count = user_count // 1000 + 1 if user_count % 1000 != 0 else user_count // 1000
     user_set = set()
     query_list = [f"API.groups.getMembers({{'group_id':{group_id},'offset':{offset}}})"
                   for offset in range(0, offset_count * 1000, 1000)]
     query_list = cutter(query_list, 25)
-    response = asyncio.run(process_execute(query_list, tokens[1]))
+    response = asyncio.run(process_execute(query_list, tokens))
     for data in response:
         for items in data['response']:
             user_set.update(items['items'])
@@ -64,28 +71,29 @@ def get_users_groups_members_count(group_id: int, tokens: list):
     query_list = [f"API.groups.get({{'user_id':{user},'extended':1,'fields':'members_count'}})"
                   for user in user_list]
     query_list = cutter(query_list, 25)
-    groups = []
-    response = asyncio.run(process_execute(query_list, tokens[1]))
+    groups_list = []
+    response = asyncio.run(process_execute(query_list, tokens))
     print(f"async task completed for {round(perf_counter() - start_time, 3)} sec")
     for data in response:
         for user in data['response']:
             if not user:
                 continue
-            for item in user['items']:
-                groups.append(item)
-    print(f"groups collected for {round(perf_counter() - start_time, 3)} sec")
-    pool = mp.Pool(mp.cpu_count())
-    mp_results = pool.map(process_data, [item for item in groups])
-    pool.close()
-    print(f"multiprocess task completed for {round(perf_counter() - start_time, 3)} sec")
-    groups.clear()
-    pd.DataFrame(mp_results, columns=['id', 'name', 'members_count']).set_index('id').to_csv("groups_member_count.csv")
-    mp_results.clear()
+            for groups in user['items']:
+                for group in groups:
+                    if 'deactivated' in group or group['is_closed'] != 0:
+                        groups_list.append((group['id'], group['name'], -1))
+                    elif 'members_count' not in group:
+                        groups_list.append((group['id'], group['name'], -2))
+                    else:
+                        groups_list.append((group['id'], group['name'], group['members_count']))
+    df = pd.DataFrame(groups_list, columns=['id', 'name', 'members_count']).set_index('id').to_csv("groups_member_count.csv")
     print(f"all task completed for {round(perf_counter() - start_time, 3)} sec")
-    return groups
+    return df
 
 
 if __name__ == "__main__":
-    token_list = list(VK_TOKENS.values())
+    t = VK_TOKENS
     target_group_id = 197217619
-    get_users_groups_members_count(target_group_id, token_list)
+    token = "95ec26ebf54f29da7f9e6508d78a96f065b9798da64e7ae5e0baa427d7c7ce7d245c951e0157e712510ad"
+    request = f"https://api.vk.com/method/groups.getMembers?group_id={target_group_id}&access_token={token}&v=5.131"
+    print(requests.get(request).json())
