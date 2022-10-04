ac29034286c26b4e40119088e1bf82a9d6e206c8
Baster <Baster@DESKTOP-FFTVG2S>
2022-10-05 02:01:51 +0600

    Dash css 2

--- Web_analysis/vk_group_dash.py
+++ Web_analysis/vk_group_dash.py
@@ -10,6 +10,8 @@ with open(f"{target_group_id}.json", "r") as f:
     group_metadata = json.loads(f.read())
 
 group_users = pd.read_csv("hololivepics_group_users.csv", sep=";")
+group_users['active'] = group_users['active'].apply(lambda x: True if x == "t" else False)
+group_users['is_closed'] = group_users['is_closed'].apply(lambda x: True if x == "t" else False)
 
 users_friends = pd.read_csv("hololivepics_users_friends.csv", sep=";")
 users_friends.users_friends = users_friends.users_friends.map(lambda x: x.strip("{}").split(","))
@@ -18,24 +20,60 @@ users_groups = pd.read_csv("hololivepics_users_groups.csv", sep=";", on_bad_line
 users_groups.access = users_groups.access.astype(bool)
 
 app = Dash(__name__, assets_folder=f"{os.getcwd()}/assets")
+
 pie_chart = group_users.groupby("active").count().reset_index().loc[:, ["active", "member_id"]]
 
 fig = px.pie(pie_chart, values="member_id", names="active")
 
 app.layout = html.Div([
     html.Header([
-        html.H3(["Демографический портрет сообщества"]),
+        html.H3([f"Демографический портрет сообщества ", html.Span(group_metadata['name'])]),
         html.Img(src=group_metadata["photo_200"])
     ], className="header"),
     html.Div([
         html.Div([
-            html.P(f"НАЗВАНИЕ: "),
-            html.Span(group_metadata['name']),
-            html.P(f"КОЛИЧЕСТВО УЧАСТНИКОВ: "),
-            html.Span(group_metadata['members_count']),
-            html.P(f"ОПИСАНИЕ: \n"),
-            html.Span(group_metadata['description']),
-        ], className="banner"),
+            html.P([
+                f"Количество участников сообщества: ",
+                html.Span(len(group_users['active']), style={"color": "#8be9fd"}),
+                ". Из них ",
+                html.Span(len(group_users['active'][~group_users['active']]), style={"color": "#ff5555"}),
+                " заблокировано\\удалено, ",
+                html.Span(len(group_users['is_closed'][group_users['is_closed']]), style={"color": "#ffb86c"}),
+                " закрыло доступ к странице",
+            ], style={"margin-bottom": "20px"}),
+            html.P([
+                f"Средний возраст участников сообщества ",
+                html.Span(round(group_users["age"][group_users["age"] >= 0].mean()), style={"color": "#8be9fd"}),
+                ", мединное значение ",
+                html.Span(round(group_users["age"][group_users["age"] >= 0].median()), style={"color": "#8be9fd"}),
+                ". Не указало возраст ",
+                html.Span(round(len(group_users["age"][group_users["age"] < 0]) * 100 / len(group_users["age"])),
+                          style={"color": "#ff5555"}),
+                " % пользователей",
+            ], style={"margin-bottom": "20px"}),
+            html.P([
+                f"В сообществе: ",
+                html.Span(round(len(group_users["sex"][group_users["sex"] == 2]) * 100 / len(group_users["sex"])),
+                          style={"color": "#8be9fd"}),
+                " % пользователей мужского пола, ",
+                html.Span(round(len(group_users["sex"][group_users["sex"] == 1]) * 100 / len(group_users["sex"])),
+                          style={"color": "#8be9fd"}),
+                " % пользователей женского и ",
+                html.Span(round(len(group_users["sex"][group_users["sex"] == 0]) * 100 / len(group_users["sex"]), 3),
+                          style={"color": "#ff5555"}),
+                " % пользователей не указавших пол",
+            ], style={"margin-bottom": "20px"}),
+            html.P([
+                html.Span(round(len(group_users["country"][group_users["country"] == "unknown"])
+                                * 100 / len(group_users["country"])),
+                          style={"color": "#ff5555"}),
+                " % пользователей не указало страну проживания, ",
+                html.Span(round(len(group_users["city"][group_users["city"] == "unknown"])
+                                * 100 / len(group_users["city"])),
+                          style={"color": "#ff5555"}),
+                " % пользователей не указало город проживания",
+            ], style={"margin-bottom": "20px"}),
+        ], className="members-count-banner"),
         html.Div([
             dcc.Graph(
                 className="pie-chart",
@@ -45,6 +83,7 @@ app.layout = html.Div([
         ])
     ], className="container-top")
 ])
+
 if __name__ == "__main__":
     # print(group_users.dtypes)
     # print(group_users.head())
